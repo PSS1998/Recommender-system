13ca0ce02a4cd25f640ee5509f4579ab0aeb2e89
PSS1998 <p_sadri_s@yahoo.com>
2020-03-27 17:32:05 +0430

    change from selenium to beautifulsoup

--- goodreadsRecommender.py
+++ goodreadsRecommender.py
@@ -1,40 +1,29 @@
-import time
-
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-
+import requests
+from bs4 import BeautifulSoup
 
 
 def goodreads_recommender(query):
-	options = FirefoxOptions()
-	options.add_argument("--headless")
-	driver = webdriver.Firefox(options=options)
-	driver.implicitly_wait(5)
-
-	url = "https://www.goodreads.com/"
-
-	driver.get(url)
-	driver.execute_script("window.scrollTo(0, document.body.scrollHeight/3);")
-	time.sleep(1)
-	search_box = driver.find_element_by_css_selector('#sitesearch_field')
-	search_box.send_keys(query)
-	driver.find_elements_by_css_selector('#headerSearchForm img')[1].click()
-
-	driver.find_element_by_css_selector('tr:nth-child(1) .bookTitle span').click()
-
-	driver.find_element_by_css_selector('.bookCarousel+ .seeMoreLink').click()
-
-	books = driver.find_elements_by_css_selector('.gr-d-lg-none+ div .listWithDividers .gr-h3--noMargin span , .gr-boxBottomDivider .gr-h3--noMargin span')
+	name = query
+	query = name.replace(" ", "+")
+	url = f"https://www.goodreads.com/search?query={query}"
+	r = requests.get(url).content
+	soup = BeautifulSoup(r, 'html.parser')
+	book_link = soup.select(".bookTitle")[0]['href']
+	book_link = "https://www.goodreads.com"+book_link
+	r = requests.get(book_link).content
+	soup = BeautifulSoup(r, 'html.parser')
+	similar_link = soup.select(".seeMoreLink")[0]['href']
+	r = requests.get(similar_link).content
+	soup = BeautifulSoup(r, 'html.parser')
+	books = soup.select(".gr-d-lg-none+ div .listWithDividers .gr-h3--noMargin span , .gr-boxBottomDivider .gr-h3--noMargin span")
 	book_list = []
 	count = 0
 	for book in books:
 		count += 1
 		if count%3 == 1:
 			book_list.append(book.text)
-
-	driver.quit()
-
+			
 	return book_list
-
-#print(goodreads_recommender("Angels & Demons"))
+	
+	
+#print(goodreads_recommender("ninth house"))
